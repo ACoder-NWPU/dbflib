5e4e2414fbd69f4b9a6749f4f16535c90256bd3c
yuwei.wang <yuwei.wang@bopufund.com>
2024-07-04 10:54:03 +0800

    去掉迅投demo

--- example/main.cpp
+++ example/main.cpp
@@ -50,56 +50,11 @@ void dump_demo(std::string filename)
     }
 }
 
-void xt_place_order_demo(std::string filename)
-{
-    try
-    {
-        DBaseTools::Loader loader(filename);
-        auto table = loader.load_table();
-
-        DBaseTools::TableBuilder builder(table);
-
-        builder.append_record(
-            {
-                {"ORDER_TYPE", "23"}, // 证券买入
-                {"PRICE_TYPE", "3"}, // 限价委托
-                {"MODE_PRICE", "10.5"},
-                {"STOCK_CODE", "000001"},
-                {"VOLUME", "500"},
-                {"ACCOUNT_ID", "301300110012"},
-                {"ACT_TYPE", "49"},
-                {"BROKERTYPE", "2"},
-                {"STRATEGY", ""},
-                {"NOTE", "博普bopu投资备注测试12345-_+=*&^%$#@!`~"},
-                {"NOTE1", ""},
-                {"COMPO_PATH", ""},
-                {"TRADEPARAM", ""},
-                {"COMMAND_ID", ""},
-                {"BASKETPATH", ""},
-                {"INSERTTIME", ""},
-                {"EXTRAPARAM", ""}
-            }
-        );
-
-        std::cout << table->to_debug_string() << std::endl;
-
-        DBaseTools::Dumper dumper(filename);
-        dumper.dump_all(builder.table);
-        dumper.flush();
-    }
-    catch (std::runtime_error& e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-}
-
 int main()
 {
-//    load_demo("D:/projects/DBaseTools/XT_ZJCX_Stock_20240102.dbf");
+    load_demo("D:/WeChat/WeChat Files/wxid_cn79p3oem8dl22/FileStorage/File/2024-07/trade(1).dbf");
 //    dump_demo();
 //    load_demo("C:/Users/wyw15/Desktop/pingan_pb/export_data/XT_DBF_ORDER.dbf");
-
-    xt_place_order_demo(R"(C:\Users\wyw15\Desktop\pingan_pb\export_data\XT_DBF_ORDER.dbf)");
 
     return 0;
 }
